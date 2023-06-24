320635f9b934912592041a119b753e9e57037ebf
igal2010 <igalkf@gmail.com>
2023-06-24 22:11:14 +0300

    Chat server multi login message

--- rsa.py
+++ rsa.py
@@ -1,6 +1,10 @@
 import random as rnd
 
+# RSA class for server socket encryption.
 class rsa:
+
+    # Following Miller-Rabin algorithm, the test being checked is if the prime number given is composite.
+    # It uses the fermat lesser scentence to detect only two square root numbers and verify the number is prime.
     def surely_composite(self, n, d, s, a):
         'n-1 == 2**s * d'
         x = pow(a, d, n)
@@ -12,6 +16,7 @@ class rsa:
             if x == n - 1: return False
         return True
 
+    # Following miller rabin algorithm, the higher the number of valid checks being performed, the higher the chance the number is prime.
     def detect_if_prime(self, n, number_of_rounds):    
         (d, s) = n - 1, 0
         while d % 2 == 0:
@@ -22,6 +27,7 @@ class rsa:
                 return False
         return True
 
+    # To get the asymmetric public key we need to perform a modulu function and detect a prime number which makes gcd(p1, p2) = 1
     def get_gcd_mod(self, e, phi):
         if phi == 0:
             return e, 1, 0
@@ -32,13 +38,15 @@ class rsa:
 
         return gcd, e_new, y_new
 
+    # To get the asymmetric private key we need to perform an inverse modulu function and detect a prime number which makes gcd(p1, p2) = 1
     def find_inverse(self, e, phi):
         gcd, e_inv, _ = self.get_gcd_mod(e, phi)
         if gcd == 1:
             return e_inv % phi
         else:
             return None
-        
+    
+    # A random number is picked and is tested for being prime or complex.
     def pick_random_prime_number(self, second_prime_number = -1, max_random_number = 1024):
         first_prime_number = rnd.getrandbits(1024) if max_random_number == 1024 else rnd.randint(3, max_random_number)
 
@@ -53,6 +61,7 @@ class rsa:
         
         return first_prime_number
 
+    # RSA required calculations for asymmetric key generation
     def define_prime_multiplication(self, first_prime_number, scond_prime_number):
         N = first_prime_number * scond_prime_number
         phi = (first_prime_number-1) * (scond_prime_number-1)
@@ -61,18 +70,21 @@ class rsa:
 
         return N, phi, e
     
+    # Execute everything together
     def generate_keys(self):
         N , phi, e = self.define_prime_multiplication(self.pick_random_prime_number(), self.pick_random_prime_number())
         private_key = self.find_inverse(e, phi)
         public_key = f'{N}:{e}'
         return private_key, public_key
 
+    # Encrypt with a given generated public key.
     def encrypt(self, m, n):
         message_num = int.from_bytes(m.encode(), 'big')
         encrypted_message = pow(message_num, 65537, n)
 
         return encrypted_message
 
+    # Decrypt with a given generated private key.
     def decrypt(self, c, d, n):
         decrypted_message = pow(c, d, n)
         result = decrypted_message.to_bytes((decrypted_message.bit_length() + 7) // 8, 'big').decode('utf-8')
--- server.py
+++ server.py
@@ -3,27 +3,44 @@ import websockets.server
 import rsa
 
 clients = []
-
+public_keys = []
 
 async def handler(client: websockets.server.WebSocketServerProtocol, path: str) -> None:
     clients.append(client)
     await client.send("Logging in to chat ..")
+
+    # Generate keys and retry on failure
     while True:
         try:
             private_key, public_key = rsa.rsa().generate_keys()
             break
         except:
             print("retry..")
+
+    # Greet a user with their private key.
     await client.send(f"Welcome, your private key is: {private_key}")
+
+    # send the new user all the logged in public keys
+    for u, k in public_keys:
+        await client.send(f'{u} logged in. Their public key is: ' + k)
+
+    # messages from user to user.
     while True:
         try:
             message = await client.recv()
         except:
             print(f'{client.host} couldn\'t send a message')
+            clients.remove(client)
+            break
         print(message)
+
+        # If the message was sent by the user.
         if ":" in message:
             user, mid, mtype, m = tuple(message.split(':'))
-        
+        else:
+            public_keys.append((message, public_key))
+
+        # Broadcast the message to all the connected clients
         for connected_client in clients:
             if connected_client.open:
                 if ":" in message:
@@ -31,12 +48,13 @@ async def handler(client: websockets.server.WebSocketServerProtocol, path: str) 
                 else:
                     await connected_client.send(f'{message} logged in. Their public key is: ' + public_key)
             else:
+                # Remove client if disconnected
                 clients.remove(connected_client)
 
 
 async def main():
     async with websockets.serve(handler, "localhost", 12345):
-        await asyncio.Future()  # run forever
+        await asyncio.Future()  # Run on a thread
 
 
 if __name__ == "__main__":
